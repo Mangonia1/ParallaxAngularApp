8d2d16fd14d5e0a89fc828d3ea9d1dc1b3ff9a0b
Antoniohdz99 <ricardo_antonyo@hotmail.com>
2019-12-15 16:36:10 -0600

    cambios verges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { CuestionariosModule } from './cuestionarios/cuestionarios.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,8 @@ import { CuestionariosModule } from './cuestionarios/cuestionarios.module';
     BrowserModule,
     AppRoutingModule,
     UsuariosModule,
-    CuestionariosModule
+    CuestionariosModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
