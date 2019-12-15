0fe62c6a3a9ce8d3f60790ff92bb4a42de371a58
EquisFuture <oscar.olveda@live.com>
2019-12-15 17:20:20 -0600

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { CuestionariosModule } from './cuestionarios/cuestionarios.module';
-<<<<<<< HEAD
 import { NavbarComponent } from './navbar/navbar.component';
-=======
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
->>>>>>> 27b6cc01d1b339e651fc6029dffb3b2fae16c12c
 
 @NgModule({
   declarations: [
