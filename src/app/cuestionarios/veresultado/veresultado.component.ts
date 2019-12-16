9e6466c4b8327c83143013dcb4fdd2f5383ffbdd
EquisFuture <oscar.olveda@live.com>
2019-12-16 16:19:20 -0600

    reporter

--- src/app/cuestionarios/veresultado/veresultado.component.ts
+++ src/app/cuestionarios/veresultado/veresultado.component.ts
@@ -6,8 +6,9 @@ import {Router} from '@angular/router';
 import { Pregunta } from '../preguntas.model';
 import { UsuariosService } from '../../usuarios/usuarios.service';
 import { FormBuilder,FormGroup,Validators,ReactiveFormsModule, FormControl } from '@angular/forms';
-import * as jsPDF from 'jspdf';
- import html2canvas from 'html2canvas'; 
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-veresultado',
@@ -118,25 +119,76 @@ this.cuestionariosservice.currentMessage3.subscribe(message =>{
              }
 
 
-  generar()  
-  {
-   
-    // console.log('mira entrooo');
-    var data = document.getElementById('listainfo');  
-    html2canvas(data).then(canvas => {  
-      // Few necessary setting options  
-      var imgWidth = 208;   
-      var pageHeight = 295;    
-    var imgHeight = canvas.height * imgWidth / canvas.width;  
-      var heightLeft = imgHeight;  
-  
-      const contentDataURL = canvas.toDataURL('image/jpeg')  
-      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
-      var position = 0;  
-      pdf.addImage(contentDataURL, 'JPEG', 0, position, imgWidth, imgHeight)  
-      pdf.save('reporte.pdf'); // Generated PDF   
-    });   
+  generar(){
+    var headRows = function headRows() {
+        return [{ No: '#', Reactivo: 'Pregunta', Respuesta: 'Respuesta' }];
+    }
 
+    var p = this.preguntas;
+    var bodyRows = function bodyRows() {
+      let body = [];
+      for (var j = 1; j <= p.length; j++) {
+          body.push({
+              No: j,
+              Reactivo: p[(j - 1)].preguntatxt,
+              Respuesta: p[(j - 1)].respuesta,
+          });
+      }
+      return body;
+    }
+
+    let doc = new jsPDF;
+    var totalPagesExp = '{total_pages_count_string}';
+
+    // variables para usarse dentro del documento
+    var empresa = this.cuesnomempresa;
+    var fecha = this.cuesfecha;
+    var auditor = this.cuesnomauditor;
+    var resultado = this.cuesresultado;
+
+    doc.autoTable({
+      head: headRows(),
+      body: bodyRows(),
+      styles: {
+          overflow: 'linebreak',
+      },
+      columnStyles: { 0: { columnWidth: 8 }, 1: { columnWidth: 110 } },
+      didDrawPage: function(data) {
+          // Header
+          doc.setFontSize(15);
+          doc.setTextColor(40);
+          doc.setFontStyle('normal');
+
+          doc.text("Parallax Auditorías", data.settings.margin.left + 65, 10);
+          doc.setFontSize(10);
+          doc.text(`Fecha de realización: `+ fecha, data.settings.margin.left + 5, 18);
+          doc.text(`Reporte de auditoria a la empresa `+ empresa, data.settings.margin.left + 5, 22);
+          doc.text(`Auditor: `+auditor, data.settings.margin.left + 5, 26);
+
+          doc.text(`Resultado de la auditoría: `+resultado, data.settings.margin.left + 5, 30);
+          // Footer
+          var str = "Página " + doc.internal.getNumberOfPages()
+              // Total page number plugin only available in jspdf v1.0+
+          if (typeof doc.putTotalPages === 'function') {
+              str = str + " of " + totalPagesExp;
+          }
+          doc.setFontSize(10);
+
+          // jsPDF 1.4+ uses getWidth, <1.4 uses .width
+          var pageSize = doc.internal.pageSize;
+          var pageHeight = pageSize.height ? pageSize.height : pageSize.getHeight();
+          doc.text(str, data.settings.margin.left, pageHeight - 10);
+      },
+      margin: { top: 40 }
+    });
+
+
+    // Total page number plugin only available in jspdf v1.0+
+    if (typeof doc.putTotalPages === 'function') {
+      doc.putTotalPages(totalPagesExp);
+    }
+
+    doc.save(`${this.cuesnomempresa}.pdf`);
   }  
 
   ngOnDestroy(): void {
